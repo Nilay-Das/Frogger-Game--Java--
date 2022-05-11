88a51cd964e96b2d6d41f9d0adb4eeb9f59fb47c
Nilay Das <104909439+Nilay-Das@users.noreply.github.com>
2022-05-11 23:59:13 +0600

    Add files via upload

---
+++ Graphics/FroggerGameRunner.java
@@ -0,0 +1,29 @@
+package Graphics;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ *
+ * Runner class for the Frogger game.
+ *
+ * @author Nilay Das
+ */
+public class FroggerGameRunner extends JPanel {
+
+    private FroggerGameGraphics chickenGame;
+    private static final Point START_POSITION = new Point(300,500);
+
+    public static void main(String[] args) throws InterruptedException {
+
+        JFrame gameFrame = new JFrame("The Frogger Game");
+        FroggerGameGraphics runner = new FroggerGameGraphics();
+        gameFrame.add(runner);
+        gameFrame.setSize(600,600);
+        gameFrame.setVisible(true);
+        gameFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        runner.graphicsGameLoop(gameFrame);
+
+    }
+
+}
